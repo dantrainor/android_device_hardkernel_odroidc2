cf9c1d975fd3f6fa750f0b50aca86d45da16593b
Dan Trainor <dan.trainor@gmail.com>
2016-03-26 13:06:43 -0700

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,19 @@
+# stuff for integrated kernel
+TARGET_KERNEL_SOURCE := kernel/hardkernel/odroidc2
+TARGET_KERNEL_CONFIG := odroidc2_defconfig
+BOARD_USES_UBOOT := true
+
+# variabels required according to https://wiki.cyanogenmod.org/w/Doc:_porting_intro#BoardConfig.mk
+TARGET_ARCH := arm64
+BOARD_KERNEL_CMDLINE := "root=/dev/mmcblk0p2 rw init=/init rootwait console=ttyS0,115200 hdmimode=1440x900p60hz hdmitx=cecf vout=hdmi overscan_top=0 overscan_left=0 overscan_bottom=0 overscan_right=0 logo=osd1,loaded,0x3f800000,1440x900p60hz androidboot.hardware=odroidc2 androidboot.serialno=HKC213254DFCCEF4 androidboot.selinux=disabled"
+BOARD_KERNEL_PAGESIZE := 4096
+
+# whole values e.g. MB GB etc seen at https://events.linuxfoundation.org/images/stories/pdf/lf_abs12_zores.pdf
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16MB
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12MB
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1GB
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4G
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+
---
+++ cm.mk
@@ -0,0 +1,6 @@
+PRODUCT_NAME := cm_odroidc2
+PRODUCT_DEVICE := odroidc2
+PRODUCT_BRAND := CyanogenMod
+PRODUCT_MODEL := odroidc2
+TARGET_CPU_ABI := armeabi
+TARGET_ARCH_VARIANT := armv8-a

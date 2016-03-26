b3624e51058baec0f67ec01ca7c178701095b46b
Dan Trainor <dan.trainor@gmail.com>
2016-03-26 13:40:20 -0700

    modify TARGET_CPU_API to what's referenced at http://odroid.com/dokuwiki/doku.php?id=en:c2_building_kernel

--- cm.mk
+++ cm.mk
@@ -2,5 +2,5 @@ PRODUCT_NAME := cm_odroidc2
 PRODUCT_DEVICE := odroidc2
 PRODUCT_BRAND := CyanogenMod
 PRODUCT_MODEL := odroidc2
-TARGET_CPU_ABI := armeabi
+TARGET_CPU_ABI := aarch64-linux-gnu
 TARGET_ARCH_VARIANT := armv8-a
